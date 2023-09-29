7ac6c40f6d977106eb5c966b22ee8131becd3989
Vladimir <kotopes231@gmail.com>
2023-09-29 12:09:36 +0300

    refactor

--- src/Extension.php
+++ src/Extension.php
@@ -15,11 +15,6 @@ class Extension extends OperationExtension
 {
     public static array $hooks = [];
 
-    public static function hook(\Closure $cb)
-    {
-        self::$hooks[] = $cb;
-    }
-
     /**
      * @return Feature[]
      */
@@ -28,25 +23,66 @@ class Extension extends OperationExtension
         return [
             new Feature(
                 Feature::AllowedIncludesMethod,
-                $this->queryParameterKey('include'),
+                config('query-builder.parameters.include'),
                 ['posts', 'posts.comments', 'books']
             ),
             new Feature(
                 Feature::AllowedFiltersMethod,
-                $this->queryParameterKey('filter'),
+                config('query-builder.parameters.filter'),
                 ['[name]=john', '[email]=gmail']
             ),
             new Feature(
                 Feature::AllowedSortsMethod,
-                $this->queryParameterKey('sort'),
+                config('query-builder.parameters.sort'),
                 ['title', '-title', 'title,-id']
             ),
             new Feature(
                 Feature::AllowedFieldsMethod,
-                $this->queryParameterKey('fields'),
+                config('query-builder.parameters.fields'),
                 ['id', 'title', 'posts.id']
             ),
         ];
+    }
+
+    public function handle(Operation $operation, RouteInfo $routeInfo)
+    {
+        foreach ($this->features() as $feature) {
+
+            /** @var Node\Expr\MethodCall $methodCall */
+            $methodCall = (new NodeFinder())->findFirst(
+                $routeInfo->methodNode(),
+                fn (Node $node) =>
+                    // todo: check if the methodName is called on QueryBuilder
+                    $node instanceof Node\Expr\MethodCall &&
+                        $node->name instanceof Node\Identifier &&
+                        $node->name->name === $feature->getMethodName()
+            );
+
+            if (! $methodCall) {
+                return;
+            }
+
+            $parameter = new Parameter($feature->getQueryParameterKey(), 'query');
+
+            $parameter->setSchema(
+                Schema::fromType(new StringType())
+            );
+
+            $feature->setValues($this->getValues($methodCall));
+
+            $parameter->example($feature->getExample());
+
+            $halt = $this->runHooks($operation, $parameter, $feature);
+
+            if (! $halt) {
+                $operation->addParameters([$parameter]);
+            }
+        }
+    }
+
+    public static function hook(\Closure $cb)
+    {
+        self::$hooks[] = $cb;
     }
 
     public function runHooks(Operation $operation, Parameter $parameter, Feature $feature): mixed
@@ -61,73 +97,27 @@ class Extension extends OperationExtension
         return false;
     }
 
-    public function handle(Operation $operation, RouteInfo $routeInfo)
+    public function getValues(Node\Expr\MethodCall $methodCall): array
     {
-        foreach ($this->features() as $feature) {
-
-            /** @var Node\Expr\MethodCall $methodCall */
-            $methodCall = (new NodeFinder())->findFirst(
-                $routeInfo->methodNode(),
-                $this->methodPredicate($feature->getMethodName()));
-
-            if (! $methodCall) {
-                return;
-            }
-
-            $parameter = new Parameter($feature->getQueryParameterKey(), 'query');
-
-            $parameter->setSchema(
-                Schema::fromType(new StringType())
-            );
-
-            if (count($methodCall->args) > 0) {
-                if ($this->isArrayParameter($methodCall)) {
-                    $values = array_map(function (Node\Expr\ArrayItem $item) {
-                        return $item->value->value;
-                    }, $methodCall->args[0]->value->items);
-
-                    $feature->setValues($values);
-                } elseif ($this->isDynamicParameters($methodCall)) {
-                    $values = array_map(function (Node\Arg $arg) {
-                        return $arg->value->value;
-                    }, $methodCall->args);
-
-                    $feature->setValues($values);
-                }
-            }
-
-            $parameter->example($feature->getExample());
-
-            $halt = $this->runHooks($operation, $parameter, $feature);
-
-            if (! $halt) {
-                $operation->addParameters([$parameter]);
-            }
+        // ->allowedIncludes()
+        if (count($methodCall->args) === 0) {
+            return [];
         }
-    }
 
-    public function isArrayParameter(Node\Expr\MethodCall $methodCall): bool
-    {
-        return $methodCall->args[0]->value instanceof Node\Expr\Array_;
-    }
+        // ->allowedIncludes(['posts', 'posts.author'])
+        if ($methodCall->args[0]->value instanceof Node\Expr\Array_) {
+            return array_map(function (Node\Expr\ArrayItem $item) {
+                return $item->value->value;
+            }, $methodCall->args[0]->value->items);
+        }
 
-    public function isDynamicParameters(Node\Expr\MethodCall $methodCall): bool
-    {
-        return $methodCall->args[0]->value instanceof Node\Scalar\String_;
-    }
+        // ->allowedIncludes('posts', 'posts.author')
+        if ($methodCall->args[0]->value instanceof Node\Scalar\String_) {
+            return array_map(function (Node\Arg $arg) {
+                return $arg->value->value;
+            }, $methodCall->args);
+        }
 
-    public function methodPredicate($methodName): \Closure
-    {
-        return function (Node $node) use ($methodName) {
-            // todo: check if the methodName is called on QueryBuilder
-            return $node instanceof Node\Expr\MethodCall &&
-                $node->name instanceof Node\Identifier &&
-                $node->name->name === $methodName;
-        };
-    }
-
-    public function queryParameterKey($featureKey)
-    {
-        return config("query-builder.parameters.$featureKey");
+        return [];
     }
 }
