155958f8689253f49e4e9a25fe6a328483eb0a35
udawadi007 <uttamdawadi@wiseberry.com.au>
2024-08-07 11:23:53 +1000

    Added missing object type on short

--- src/AllowedSortsExtension.php
+++ src/AllowedSortsExtension.php
@@ -8,24 +8,28 @@ use Dedoc\Scramble\Support\Generator\Operation;
 use Dedoc\Scramble\Support\Generator\Parameter;
 use Dedoc\Scramble\Support\Generator\Schema;
 use Dedoc\Scramble\Support\Generator\Types\ArrayType;
+use Dedoc\Scramble\Support\Generator\Types\ObjectType;
 use Dedoc\Scramble\Support\Generator\Types\StringType;
 use Dedoc\Scramble\Support\RouteInfo;
 
-class AllowedSortsExtension extends OperationExtension {
+class AllowedSortsExtension extends OperationExtension
+{
     use Hookable;
+
     const MethodName = 'allowedSorts';
     public array $examples = ['title', '-title', 'title,-id'];
     public string $configKey = 'query-builder.parameters.sort';
+
     public function handle(Operation $operation, RouteInfo $routeInfo)
     {
         $helper = new InferHelper();
 
         $methodCall = Utils::findMethodCall($routeInfo, self::MethodName);
 
-        if(!$methodCall) {
+        if (!$methodCall) {
             return;
         }
-        
+
         $values = $helper->inferValues($methodCall, $routeInfo);
         $arrayType = new ArrayType();
         $arrayType->items->enum(array_merge(
@@ -33,15 +37,15 @@ class AllowedSortsExtension extends OperationExtension {
             array_map(fn($value) => '-' . $value, $values)
         ));
 
-              $objectType = new ObjectType();
-        foreach($arrayType->items->enum as $value) {
+        $objectType = new ObjectType();
+        foreach ($arrayType->items->enum as $value) {
             $objectType->addProperty($value, new StringType());
         }
         $parameter = new Parameter(config($this->configKey), 'query');
         $parameter->setSchema(Schema::fromType($objectType))->example($this->examples);
 
         $halt = $this->runHooks($operation, $parameter);
-        if (! $halt) {
+        if (!$halt) {
             $operation->addParameters([$parameter]);
         }
     }
